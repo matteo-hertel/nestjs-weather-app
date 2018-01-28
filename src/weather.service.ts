a316522d65a5d33c3cd5c6031e24c5b052921a4b
Matteo Hertel <info@matteohertel.com>
2018-01-28 11:11:09 +0000

    add weather service

---
+++ src/weather.service.ts
@@ -0,0 +1,23 @@
+import { Component } from "@nestjs/common";
+import axios, { AxiosInstance } from "axios";
+
+@Component()
+export class WeatherService {
+  private client: AxiosInstance;
+
+  constructor() {
+    this.client = axios.create({
+      baseUrl: "https://api.openweathermap.org/data/2.5/",
+      params: {
+        APPID: process.env.API_KEY
+      }
+    });
+  }
+
+  async ofCity(city: string): object {
+    const response = await this.client.get("weather", {
+      params: { q: city }
+    });
+    return response.data;
+  }
+}
