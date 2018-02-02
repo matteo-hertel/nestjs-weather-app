55097a54e000dda657a568b4c9a2f37817e8109b
Matteo Hertel <info@matteohertel.com>
2018-02-02 19:20:50 +0000

    finish weather controller

--- src/weather.controller.ts
+++ src/weather.controller.ts
@@ -1,1 +1,13 @@
-import { Get, Controller } from "@nest/common";
+import { Get, Controller } from "@nestjs/common";
+import { WeatherService } from "./weather.service";
+
+@Controller("weather")
+export class WeatherController {
+  constructor(private readonly weatherService: WeatherService) {}
+
+  @Get()
+  async root(): Promise<object> {
+    const response = await this.weatherService.forCity("New York");
+    return response;
+  }
+}
